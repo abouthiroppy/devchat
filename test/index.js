fc370256b0a46695f71d297db3d08dfd3a2a2bd4
Yuta Hiroto <git@about-hiroppy.com>
2017-06-21 08:15:43 +0900

    test: add test suites

--- test/index.js
+++ test/index.js
@@ -1,14 +1,41 @@
 import test from 'ava';
 import devchat from '../';
 
-test('e2e', async (t) => {
+test('should return categories', (t) => {
+  const categories = devchat.getCategories();
+
+  t.is(typeof categories, 'object');
+  t.is(Object.keys(categories).length, 9);
+
+});
+
+test('e2e', async(t) => {
   const category = 'jsJabber';
 
   const podCasts = await devchat.getPodCasts(category);
 
-  const { path } = podCasts[0];
+  t.not(podCasts.length, 0);
 
-  const podCast = await devchat.getPodCast(category, path);
+  const podCast = podCasts[0];
+  t.not(podCast.num, '');
+  t.not(podCast.path, '');
+  t.not(podCast.title, '');
+  t.not(podCast.duration, '');
+  t.not(podCast.publishedDate, '');
 
-  console.log(podCast)
+  // t.not(podCast.panel, []);
+
+  const { path } = podCast;
+
+  const detail = await devchat.getPodCast(category, path);
+
+  t.not(detail.src, '');
+  t.not(detail.uri, '');
+  t.not(detail.num, '');
+  t.not(detail.title, '');
+  t.not(detail.duration, '');
+  t.not(detail.publishedDate, '');
+
+  // t.not(detail.panel, []);
+  // t.not(detail.downloadLink, '');
 });
